9c6ca9bb8bb760f1a2571b0e9dd153aac594f4fa
mickypuspa <miqueltorrescampo@gmail.com>
2020-04-27 19:21:07 +0200

    Prefabs y Scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager boardScript;
+
+    private void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+    }
+
+    private void Start()
+    {
+        boardScript.SetupScene();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+
+
+}
