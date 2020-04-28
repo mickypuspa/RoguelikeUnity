fbe2a174f45526448b362af8d30119842a96cb65
mickypuspa <miqueltorrescampo@gmail.com>
2020-04-28 19:26:39 +0200

    MovingObject1

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+
+    private float movementSpeed;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Start()
+    {
+        movementSpeed = 1f /moveTime;
+        
+    }
+
+    protected IEnumerator SmoothMovement(Vector2 end)
+    {
+        float remainingDistance = Vector2.Distance(rb2D.position, end);
+
+        while (remainingDistance > float.Epsilon)
+        {
+            Vector2 newPosition = Vector2.MoveTowards(rb2D.position, end, movementSpeed * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            remainingDistance = Vector2.Distance(rb2D.position, end);
+            yield return null;
+        }
+    }
+
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if(hit.transform  == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
+    protected abstract void OnCantMove(GameObject go);
+    
+    protected virtual void AttemptMove(int xDir, int yDir)
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+        if (canMove) return;
+
+        OnCantMove(hit.transform.gameObject);
+    }
+}
