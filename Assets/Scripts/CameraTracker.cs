9968c1a30c2cc9ad756dd8ca2b5055f5a9953a54
mickypuspa <miqueltorrescampo@gmail.com>
2020-05-03 11:28:00 +0200

    nueva camara

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    public Transform trackingTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trackingTarget != null)
+        {
+            transform.position = new Vector3(trackingTarget.position.x,
+             trackingTarget.position.y, transform.position.z);
+        }
+    }
+}
